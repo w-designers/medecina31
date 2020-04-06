076b9abcf7afd25c3bd7c577cacbd89b42f96a38
vladimir-skikh <19vladimir99@mail.ru>
2020-04-06 23:26:36 +0300

    Стили для адаптива Вова

--- js/script.js
+++ js/script.js
@@ -1,10 +1,43 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  if (document.body.clientWidth > 900) {
+    $('.doctors-list').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      dots: true,
+      prevArrow: "<img src='/img/left-arrow.svg' class='prev slick-arrow' alt='предыдущая'>",
+      nextArrow: "<img src='/img/right-arrow.svg' class='next slick-arrow' alt='следующая'>",
+    });
+  }
+  else if ((document.body.clientWidth < 900) && (document.body.clientWidth > 768)) {
+    $('.doctors-list').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: true,
+      prevArrow: "<img src='/img/left-arrow.svg' class='prev slick-arrow' alt='предыдущая'>",
+      nextArrow: "<img src='/img/right-arrow.svg' class='next slick-arrow' alt='следующая'>",
+    });
+  }  
+  else if ((document.body.clientWidth < 768) && (document.body.clientWidth > 576)) {
+    $('.doctors-list').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: true,
+      prevArrow: "<img src='/img/left-arrow.svg' class='prev slick-arrow' alt='предыдущая'>",
+      nextArrow: "<img src='/img/right-arrow.svg' class='next slick-arrow' alt='следующая'>",
+    });
+  }  
+  else if (document.body.clientWidth < 576) {
   $('.doctors-list').slick({
     infinite: true,
-    slidesToShow: 4,
+    slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
     prevArrow: "<img src='/img/left-arrow.svg' class='prev slick-arrow' alt='предыдущая'>",
     nextArrow: "<img src='/img/right-arrow.svg' class='next slick-arrow' alt='следующая'>",
   });
+  }  
 });
+
